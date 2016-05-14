66686347a987a93061f74d869973f39c6b9f4434
Irek <irekwrobel23@gmail.com>
2016-05-14 17:43:46 +0200

    Added simple version of iterator

--- SinglyLinkedList.hpp
+++ SinglyLinkedList.hpp
@@ -1,16 +1,93 @@
+/*void myAssert( int a, const string& msg )
+{
+    if( !a )
+    {
+        cerr << msg << " ... Press 'Enter' to exit ... " << flush;
+        cin.clear(); cin.sync(); cin.get(); exit(1);
+    }
+}*/
+
+template < class T > class Iter;
+template < class T > class SinglyLinkedList;
+template < class T > class Node;
+
+template < class T >
+class Iter {
+
+public:
+    Iter() : current( NULL ) {}
+
+    // return T value at Node of this Iter  ...
+    T operator * () const {
+        assert( current != NULL);
+        return current-> data;
+    }
+    T& operator * () {
+        assert( current != NULL);
+        return current-> data;
+    }
+    // pre ++ ...
+    Iter& operator ++ () {
+        assert( current != NULL);
+        current = current-> next;
+        return *this;
+    }
+    // post ++ ...
+    Iter& operator ++ ( int anIntDummyHereToIndicatePostPP ) {
+        assert( current != NULL);
+        current = current-> next;
+        return *this;
+    }
+    
+    bool operator == ( Iter< T > it ) const { return current == it.current; }
+    bool operator != ( Iter< T > it ) const { return current != it.current; }
+
+private:
+    Node< T >* current;
+    friend class List< T >;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 template < class T >
 
 class SinglyLinkedList {
 
+public:
+	typedef T value_type;
+	typedef value_type* pointer;
+	typedef const value_type* const_pointer;
+	typedef value_type* iterator;
+	typedef const value_type* const_iterator;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+	typedef reverse_iterator< const_iterator > const_reverse_iterator;
+	typedef reverse_iterator< iterator > reverse_iterator;
+
+
 private:
 	class Node {
 		friend class SinglyLinkedList< T >;
+		friend class Iter< T >;
 
 		private:
 			T data;
 			Node* next;
 		public:
 			Node( T d, Node* n = NULL ) : data( d ), next( n ) {}
+			
 	};
 
 	Node* head;
@@ -38,6 +115,21 @@ public:
 	bool empty() {
 		return count == 0;
 	}
+
+	iterator begin() { return head };
+	const_iterator cbegin() const { return head; };
+	iterator end() { return tail };
+	const_iterator cend() const { return tail; };
+	reverse_iterator rbegin() { return reverse_iterator( end() ); }
+	const_reverse_iterator crbegin() const { return const_reverse_iterator( end() ); }
+	reverse_iterator rend() { return reverse_iterator( begin() ); }
+	const_reverse_iterator crend() const { return const_reverse_iterator( begin() ); }
+	size_type size() const { return size_type( end() - begin() ); }
+	size_type max_size() const { return size_type() / sizeof( T ); }
+	//bool empty() const { return begin() == end() }
+
+
+
 
 };
 
@@ -119,4 +211,13 @@ SinglyLinkedList< T >::SinglyLinkedList( const SinglyLinkedList< T >& source ) :
 		this-> push_back( current-> data )
 		current = current-> next;
 	}
+}
+
+template < class T >
+SinglyLinkedList& SinglyLinkedList< T >::operator = ( const SinglyLinkedList < T > & source ) {
+	if ( this != &source ) {
+		//wyczysc
+		for( Node< T )
+	}
+	return *this;
 }
